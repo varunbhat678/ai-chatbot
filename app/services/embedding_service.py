0e7334a0660f81ec5598cd8c5f5035cf77d350df
varunbhat678 <bhatvarun37@gmail.com>
2026-09-06 01:38:34 +0530

    Lazy load embedding model

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,10 +1,20 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+embeddings = None
+
+
+def get_embeddings():
+    global embeddings
+
+    if embeddings is None:
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    return embeddings
 
 
 def create_embeddings(chunks):
-    return embeddings.embed_documents(chunks)
+
+    return get_embeddings().embed_documents(chunks)
